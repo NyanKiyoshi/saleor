a14d2f34a826a3285c3fe2b17cb810e0ae387f87
dominik-zeglen <flesz3@o2.pl>
2018-04-12 10:46:27 +0200

    wip

--- saleor/static/dashboard-next/products/queries.ts
+++ saleor/static/dashboard-next/products/queries.ts
@@ -1,7 +1,12 @@
 import gql from "graphql-tag";
 import { Query, QueryProps } from "react-apollo";
 
-import { ProductListQuery, ProductListQueryVariables } from "../gql-types";
+import {
+  ProductListQuery,
+  ProductListQueryVariables,
+  ProductDetailsQuery,
+  ProductDetailsQueryVariables
+} from "../gql-types";
 
 export const TypedProductListQuery = Query as React.ComponentType<
   QueryProps<ProductListQuery, ProductListQueryVariables>
@@ -9,6 +14,33 @@ export const TypedProductListQuery = Query as React.ComponentType<
 export const productListQuery = gql`
   query ProductList($first: Int, $after: String, $last: Int, $before: String) {
     products(before: $before, after: $after, first: $first, last: $last) {
+      edges {
+        node {
+          id
+          name
+          thumbnailUrl
+          productType {
+            id
+            name
+          }
+        }
+      }
+      pageInfo {
+        hasPreviousPage
+        hasNextPage
+        startCursor
+        endCursor
+      }
+    }
+  }
+`;
+
+export const TypedProductDetailsQuery = Query as React.ComponentType<
+  QueryProps<ProductDetailsQuery, ProductDetailsQueryVariables>
+>;
+export const productDetailsQuery = gql`
+  query ProductDetails($id: ID!) {
+    product(id: $id) {
       edges {
         node {
           id
--- saleor/static/dashboard-next/products/views/ProductDetails.tsx
+++ saleor/static/dashboard-next/products/views/ProductDetails.tsx
@@ -7,104 +7,11 @@ import { productStorefrontUrl } from "../";
 import Navigator from "../../components/Navigator";
 import ProductCollections from "../components/ProductCollections";
 import ProductDescription from "../components/ProductDescription";
+import ProductDetailsPage from "../components/ProductDetailsPage";
 import ProductImages from "../components/ProductImages";
 import ProductPriceAndAvailability from "../components/ProductPriceAndAvailability";
 import ProductVariants from "../components/ProductVariants";
-
-const product = {
-  attributes: '{"1": "1", "2": "4", "3": "6"}',
-  availability: {
-    available: true,
-    onSale: true
-  },
-  availableOn: null,
-  category: {
-    id: "Q2F0ZWdvcnk6MQ=="
-  },
-  slug: "medina-barnes",
-  collections: [
-    {
-      id: "UHSJasdjsatJSAj=",
-      name: "Winter collection"
-    },
-    {
-      id: "UHSJasdjsatJSAj=",
-      name: "Evil developer's choice"
-    }
-  ],
-  description:
-    "Challenge carry create daughter debate. Amount leader college where raise. Pass nor energy that television job.\n\nLeft five personal figure daughter. Actually company page include light summer people. Thank why consider car. Federal community community activity region during.\n\nTurn specific data. Knowledge chair win star. Always thus part hot it drug quickly current.\n\nVarious dog safe son often. President season military happy situation none medical.\n\nBetween answer ball actually include no hand.",
-  id: "UHJvZHVjdDox",
-  images: [
-    {
-      id: "UHJvZHVjdEltYWdlOjE=",
-      alt: "",
-      order: 0,
-      url: "/media/products/saleor/static/placeholders/t-shirts/5_cLzVLiR.jpg"
-    },
-    {
-      id: "UHJvZHVjdEltYWdlOjI=",
-      alt: "",
-      order: 1,
-      url: "/media/products/saleor/static/placeholders/t-shirts/5_5UrD4zQ.jpg"
-    },
-    {
-      id: "UHJvZHVjdEltYWdlOjM=",
-      alt: "",
-      order: 2,
-      url: "/media/products/saleor/static/placeholders/t-shirts/5_ywk5ZFz.jpg"
-    },
-    {
-      id: "UHJvZHVjdEltYWdlOjQ=",
-      alt: "",
-      order: 3,
-      url: "/media/products/saleor/static/placeholders/t-shirts/5_IqRhZqc.jpg"
-    }
-  ],
-  isPublished: true,
-  name: "Medina-Barnes",
-  price: {
-    amount: 96.65,
-    currency: "USD",
-    localized: "96,65 USD"
-  },
-  productType: {
-    id: "1"
-  },
-  updatedAt: "2018-03-27T12:35:41.198391+00:00",
-  variants: [
-    {
-      id: "123123123",
-      sku: "9481723647712",
-      name: "Color:White Material:Wool",
-      availability: true,
-      price: {
-        grossLocalized: "14 USD"
-      },
-      grossMargin: "0 USD"
-    },
-    {
-      id: "12312312w3",
-      sku: "9481723647713",
-      name: "Color:Blac Material:Wool",
-      availability: true,
-      price: {
-        grossLocalized: "15 USD"
-      },
-      grossMargin: "1 USD"
-    },
-    {
-      id: "123123123",
-      sku: "9481723647712",
-      name: "Color:Blue Material:Acryl",
-      availability: false,
-      price: {
-        grossLocalized: "12 USD"
-      },
-      grossMargin: "0 USD"
-    }
-  ]
-};
+import { productDetailsQuery, TypedProductDetailsQuery } from "../queries";
 
 const decorate = withStyles(theme => ({
   root: {
@@ -119,43 +26,6 @@ const decorate = withStyles(theme => ({
 }));
 
 export const ProductDetails = decorate(({ classes }) => (
-  <Navigator>
-    {navigate => (
-      <div className={classes.root}>
-        <div>
-          <ProductDescription
-            product={product}
-            onBack={() => window.history.back()}
-            // TODO: Replace when API is ready
-            onDelete={() => {}}
-            onEdit={id => () => navigate(productEditUrl(id))}
-            onShow={(slug: string) => () =>
-              window.open(productStorefrontUrl(slug))}
-          />
-          <Hidden smDown>
-            <ProductImages imageList={product.images} />
-          </Hidden>
-        </div>
-        <div>
-          <ProductPriceAndAvailability onPublish={event => {}} />
-          <ProductCollections
-            collections={product.collections}
-            // TODO: Replace when API is ready
-            onRowClick={id => () => navigate("#")}
-          />
-        </div>
-        <Hidden mdUp>
-          <div>
-            <ProductImages imageList={product.images} />
-          </div>
-        </Hidden>
-        <ProductVariants
-          variants={product.variants}
-          // TODO: Replace when API is ready
-          onRowClick={id => () => navigate("#")}
-        />
-      </div>
-    )}
-  </Navigator>
+  <Navigator>{navigate => <ProductDetailsPage />}</Navigator>
 ));
 export default ProductDetails;
